35dc211c5d184685a79e0f26eee960357b2b8583
git-lakshy <lakshy.smh@gmail.com>
2026-02-13 16:55:59 +0530

    auth fixes

--- src/lib/auth-actions.ts
+++ src/lib/auth-actions.ts
@@ -31,6 +31,9 @@ export async function signup(formData: FormData) {
     const data = {
         email: formData.get('email') as string,
         password: formData.get('password') as string,
+        options: {
+            emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'}/auth/callback`,
+        },
     }
 
     const { error } = await supabase.auth.signUp(data)
